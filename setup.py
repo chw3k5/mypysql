5b22f95033e936ffacea093c88583c6b24ae985c
Caleb Wheeler <chw3k5@gmail.com>
2022-11-23 15:47:30 -0700

    Removing SpExoDisks specific tables.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='mypysql',
-      version='0.1.0',
+      version='0.1.1',
       description='Load data to a MySQL database',
       author='Caleb Wheeler',
       author_email='chw3k5@gmail.com',
