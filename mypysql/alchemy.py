b06d66874cd7ba6a2b3abbcb5e7dc14b4314e43d
chw3k5 <chw3k5@gmail.com>
2023-02-09 17:27:59 -0600

    upgrade to remove most nulls in a spectrum except for large spans,
    that is when the bandwidth fraction is greater than 0.01
    or user provided value.

--- mypysql/alchemy.py
+++ mypysql/alchemy.py
@@ -1,5 +1,7 @@
-import pandas as pd
+from typing import List, Optional
+
 import numpy as np
+import pandas as pd
 import sqlalchemy as sa
 
 from mypysql.get_login import sql_host, sql_port, sql_database, sql_user,  sql_password
@@ -17,26 +19,45 @@ def is_good_num(a_float):
 null_val = np.nan
 
 
-def format_spectrum(single_spectrum):
-    if single_spectrum.flux_error is None:
-        for wavelength_um, flux in zip(single_spectrum.wavelength_um, single_spectrum.flux):
-            if is_good_num(flux):
-                yield wavelength_um, flux, null_val
-            else:
-                yield wavelength_um, null_val, null_val
-    else:
-        for wavelength_um, flux, flux_error \
-                in zip(single_spectrum.wavelength_um, single_spectrum.flux, single_spectrum.flux_error):
-            single_row_values = [wavelength_um]
-            if is_good_num(flux):
-                single_row_values.append(flux)
-            else:
-                single_row_values.append(null_val)
-            if is_good_num(flux_error):
-                single_row_values.append(flux_error)
-            else:
-                single_row_values.append(null_val)
-            yield tuple(single_row_values)
+bandwidth_fraction_for_null_default = 0.01
+
+
+def remove_bad_nums(wavelength_um: List[float], flux: List[float], flux_error: Optional[List[float]] = None):
+    if flux_error is None:
+        flux_error = [null_val] * len(flux)
+    for wavelength_um, flux, flux_error in zip(wavelength_um, flux, flux_error):
+        if is_good_num(flux):
+            if not is_good_num(flux_error):
+                flux_error = null_val
+            yield wavelength_um, flux, flux_error
+
+
+def format_spectrum(wavelength_um: List[float], flux: List[float], flux_error: Optional[List[float]] = None,
+                    bandwidth_fraction_for_null: float = bandwidth_fraction_for_null_default):
+    # replacement here to save memory in the function call
+    wavelength_um, flux, flux_error = zip(*remove_bad_nums(wavelength_um, flux, flux_error))
+    wavelength_um = np.array(wavelength_um)
+    flux = np.array(flux)
+    flux_error = np.array(flux_error)
+    wavelength_um_min = np.min(wavelength_um)
+    wavelength_um_max = np.max(wavelength_um)
+    bandwidth = wavelength_um_max - wavelength_um_min
+    bandwidth_for_null = bandwidth * bandwidth_fraction_for_null
+    wavelength_um_step = wavelength_um[1:] - wavelength_um[:-1]
+    # for large steps in wavelength, insert a null value to break up the spectrum into segments of contiguous data
+    insert_count = 0
+    for i, wavelength_um_step in enumerate(wavelength_um_step):
+        if wavelength_um_step > bandwidth_for_null:
+            insert_count += 1
+            wavelength_um_null = wavelength_um[i] + (wavelength_um_step / 2.0)
+            insert_index = i + insert_count
+            wavelength_um = np.insert(wavelength_um, insert_index, wavelength_um_null)
+            flux = np.insert(flux, insert_index, null_val)
+            flux_error = np.insert(flux_error, insert_index, null_val)
+    zipped_spectrum = list(zip(wavelength_um, flux, flux_error))
+    structured_array = np.array(zipped_spectrum,
+                                dtype=[('wavelength_um', '<f8'), ('flux', '<f8'), ('flux_error', '<f8')])
+    return structured_array
 
 
 class UploadSQL:
@@ -49,10 +70,10 @@ class UploadSQL:
     def upload_table(self, table_name, df, schema=sql_database):
         df.to_sql(table_name, con=self.engine, schema=schema, if_exists='replace', index=False)
 
-    def upload_spectra(self, single_spectrum, schema=sql_database, table_name=None):
-        structured_array = np.array(list(format_spectrum(single_spectrum)),
-                                    dtype=[('wavelength_um', '<f8'), ('flux', '<f8'), ('flux_error', '<f8')])
+    def upload_spectra(self, table_name: str, wavelength_um: List[float], flux: List[float],
+                       flux_error: Optional[List[float]] = None, schema: str = sql_database,
+                       bandwidth_fraction_for_null: float = bandwidth_fraction_for_null_default):
+        structured_array = format_spectrum(wavelength_um=wavelength_um, flux=flux, flux_error=flux_error,
+                                           bandwidth_fraction_for_null=bandwidth_fraction_for_null)
         df = pd.DataFrame(structured_array)
-        if table_name is None:
-            table_name = single_spectrum.spectrum_handle
         self.upload_table(table_name=table_name, df=df, schema=schema)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='mypysql',
-      version='0.1.2',
+      version='0.1.3',
       description='Load data to a MySQL database',
       author='Caleb Wheeler',
       author_email='chw3k5@gmail.com',
